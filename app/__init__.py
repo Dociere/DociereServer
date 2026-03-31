b3f628e0f35bbf5bd1fe186074352e64beacd026
Clint George <clintbgeorge@gmail.com>
2026-03-31 15:14:52 +0530

    Fix CORS issues
    Signed-off-by: Clint George <clintbgeorge@gmail.com>

--- app/__init__.py
+++ app/__init__.py
@@ -20,6 +20,7 @@ def create_app():
             "http://localhost:3000",
             "http://localhost:5173",
             "http://localhost:5000",
+            "https://www.dociere.com"
         ],
         allow_credentials=True,
         allow_methods=["*"],
